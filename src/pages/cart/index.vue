685339a1d02d81f29e51c4ab0d2b9cc5559b7f95
heyushuo <1007561607@qq.com>
2018-08-08 21:58:53 +0800

    cart

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -50,126 +50,131 @@
 </template>
 
 <script>
-import { get, login } from "../../utils";
-export default {
-  onShow() {
-    if (login()) {
-      this.userInfo = login();
-      this.getListData();
-    }
-  },
-  created() {
-    // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.record" 这个 scope
-    if (login()) {
-      this.userInfo = login();
-      this.getListData();
-    }
-  },
-  data() {
-    return {
-      allcheck: false,
-      listData: [],
-      Listids: [],
-      userInfo: {},
-      startX: 0,
-      startY: 0,
-      moveEndX: 0,
-      moveEndY: 0,
-      tranX: 0
-    };
-  },
-  components: {},
-  methods: {
-    startMove(e) {
-      this.startX = e.touches[0].pageX;
-      this.startY = e.touches[0].pageY;
-    },
-    deleteGoods(e) {
-      this.moveEndX = e.touches[0].pageX;
-      this.moveEndY = e.touches[0].pageY;
-      let X = this.moveEndX - this.startX;
-      let Y = this.moveEndX - this.startY;
-      if (X >= 100) {
-        X = 0;
-      }
-      if (X <= -100) {
-        X = -100;
-      }
-      this.tranX = X;
-      if (Math.abs(X) > Math.abs(Y) && X > 0) {
-        console.log("left 2 right");
-      } else if (Math.abs(X) > Math.abs(Y) && X < 0) {
-        console.log("right 2 left");
+  import {
+    get,
+    login
+  } from "../../utils";
+  export default {
+    onShow() {
+      if (login()) {
+        this.userInfo = login();
+        this.getListData();
       }
     },
-    orderDown() {
-      console.log(this.Listids.length);
+    created() {
+      // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.record" 这个 scope
+      if (login()) {
+        this.userInfo = login();
+        this.getListData();
+      }
+    },
+    data() {
+      return {
+        allcheck: false,
+        listData: [],
+        Listids: [],
+        userInfo: {},
+        startX: 0,
+        startY: 0,
+        moveEndX: 0,
+        moveEndY: 0,
+        tranX: 0
+      };
+    },
+    components: {},
+    methods: {
+      startMove(e) {
+        this.startX = e.touches[0].pageX;
+        this.startY = e.touches[0].pageY;
+      },
+      deleteGoods(e) {
+        this.moveEndX = e.touches[0].pageX;
+        this.moveEndY = e.touches[0].pageY;
+        let X = this.moveEndX - this.startX;
+        let Y = this.moveEndX - this.startY;
+        if (X >= 100) {
+          X = 0;
+        }
+        if (X <= -100) {
+          X = -100;
+        }
+        this.tranX = X;
+        if (Math.abs(X) > Math.abs(Y) && X > 0) {
+          console.log("left 2 right");
+        } else if (Math.abs(X) > Math.abs(Y) && X < 0) {
+          console.log("right 2 left");
+        }
+      },
+      orderDown() {
+        console.log(this.Listids.length);
 
-      if (this.Listids.length == 0) {
-        wx.showToast({
-          title: "请选择商品",
-          icon: "none",
-          duration: 1500
+        if (this.Listids.length == 0) {
+          wx.showToast({
+            title: "请选择商品",
+            icon: "none",
+            duration: 1500
+          });
+        }
+      },
+      async getListData() {
+        const data = await get("/cart/cartList", {
+          openId: this.userInfo.openId
         });
-      }
-    },
-    async getListData() {
-      const data = await get("/cart/cartList", {
-        openId: this.userInfo.openId
-      });
-      this.listData = data.data;
-      console.log(this.listData);
-      console.log(this.Listids);
-    },
-    allCheck() {
-      if (this.allcheck) {
-        this.allcheck = false;
-        this.Listids = [];
-      } else {
-        this.allcheck = true;
-        this.Listids = this.listData;
-      }
-    },
-    change(e) {},
-    changeColor(index) {
-      if (this.Listids[index]) {
-        this.$set(this.Listids, index, false);
-      } else {
-        this.$set(this.Listids, index, true);
-      }
-    }
-  },
-  computed: {
-    isCheckedNumber() {
-      let number = 0;
-      for (let i = 0; i < this.Listids.length; i++) {
-        if (this.Listids[i]) {
-          number++;
+        this.listData = data.data;
+        console.log(this.listData);
+        console.log(this.Listids);
+      },
+      allCheck() {
+        if (this.allcheck) {
+          this.allcheck = false;
+          this.Listids = [];
+        } else {
+          this.allcheck = true;
+          this.Listids = this.listData;
+        }
+      },
+      change(e) {},
+      changeColor(index) {
+        if (this.Listids[index]) {
+          this.$set(this.Listids, index, false);
+        } else {
+          this.$set(this.Listids, index, true);
         }
       }
-      console.log(number);
-
-      if (number == this.listData.length) {
-        console.log("zzzz");
-
-        this.allcheck = true;
-      }
-      console.log(this.allcheck);
-
-      return number;
     },
-    allPrise() {
-      var Prise = 0;
-      for (let i = 0; i < this.Listids.length; i++) {
-        if (this.Listids[i]) {
-          Prise = Prise + this.listData[i].retail_price * this.listData[i].number;
+    computed: {
+      isCheckedNumber() {
+        let number = 0;
+        for (let i = 0; i < this.Listids.length; i++) {
+          if (this.Listids[i]) {
+            number++;
+          }
         }
+        console.log(number);
+
+        if (number == this.listData.length && number != 0) {
+          console.log("zzzz");
+
+          this.allcheck = true;
+        }
+        console.log(this.allcheck);
+
+        return number;
+      },
+      allPrise() {
+        var Prise = 0;
+        for (let i = 0; i < this.Listids.length; i++) {
+          if (this.Listids[i]) {
+            Prise = Prise + this.listData[i].retail_price * this.listData[i].number;
+          }
+        }
+        return Prise;
       }
-      return Prise;
     }
-  }
-};
+  };
+
 </script>
 <style lang='scss' scoped>
-@import "./style";
+  @import "./style";
+
 </style>
