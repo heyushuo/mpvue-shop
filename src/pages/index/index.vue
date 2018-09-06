3a25a663e77fbcb79fda050d8d352ca86e660214
heyushuo <1007561607@qq.com>
2018-09-06 21:32:51 +0800

    新增调搞的地图api

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -123,107 +123,142 @@
 </template>
 
 <script>
-import amapFile from "../../utils/amap-wx";
-import { get } from "../../utils";
-export default {
-  onLoad() {
-    this.getCityName();
-  },
-  onShow() {
-    if (wx.getStorageSync("cityName")) {
-      this.cityName = wx.getStorageSync("cityName");
-    }
-  },
-  mounted() {
-    this.getData();
-  },
-  data() {
-    return {
-      cityName: "定位中..",
-      banner: [],
-      channel: [],
-      brandList: [],
-      newGoods: [],
-      hotGoods: [],
-      topicList: [],
-      newCategoryList: []
-    };
-  },
-  components: {},
-  methods: {
-    toMappage() {
-      wx.navigateTo({ url: "/pages/mappage/main" });
+  import amapFile from "../../utils/amap-wx";
+  import {
+    get
+  } from "../../utils";
+  export default {
+    onLoad() {
+      this.getCityName();
     },
-    getCityName() {
-      var _this = this;
-      var myAmapFun = new amapFile.AMapWX({ key: "e545e7f79a643f23aef187add14e4548" });
-      myAmapFun.getRegeo({
-        success: function(data) {
-          //成功回调
-          console.log(data);
-          // data[0].regeocodeData.formatted_address
-          _this.cityName = data[0].regeocodeData.formatted_address;
-        },
-        fail: function(info) {
-          //失败回调
-          console.log(info);
-          //如果用户拒绝授权
-          // 默认为北京
-          _this.cityName = "北京市";
-        }
-      });
+    onShow() {
+      if (wx.getStorageSync("cityName")) {
+        this.cityName = wx.getStorageSync("cityName");
+        wx.removeStorageSync('cityName');
+      }
     },
-    toSearch() {
-      wx.navigateTo({ url: "/pages/search/main" });
+    mounted() {
+      this.getData();
     },
-    async getData() {
-      const data = await get("/index/index");
-      this.banner = data.banner;
-      this.channel = data.channel;
-      this.brandList = data.brandList;
-      this.newGoods = data.newGoods;
-      this.hotGoods = data.hotGoods;
-      this.topicList = data.topicList;
-      this.newCategoryList = data.newCategoryList;
+    data() {
+      return {
+        cityName: "定位中..",
+        banner: [],
+        channel: [],
+        brandList: [],
+        newGoods: [],
+        hotGoods: [],
+        topicList: [],
+        newCategoryList: []
+      };
     },
-    goodsDetail(id) {
-      wx.navigateTo({
-        url: "/pages/goods/main?id=" + id
-      });
-    },
-    categoryList(id) {
-      wx.navigateTo({
-        url: "/pages/categorylist/main?id=" + id
-      });
-    },
-    goodsList(info) {
-      if (info == "hot") {
-        wx.navigateTo({
-          url: "/pages/newgoods/main?isHot=" + 1
+    components: {},
+    methods: {
+      toMappage() {
+        var _this = this;
+        // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.record" 这个 scope
+        wx.getSetting({
+          success(res) {
+            //如果没有同意授权,打开设置
+            if (!res.authSetting['scope.userLocation']) {
+              wx.openSetting({
+                success: (res) => {
+                  _this.getCityName();
+                }
+              })
+            } else {
+              wx.navigateTo({
+                url: "/pages/mappage/main"
+              });
+            }
+          }
+        })
+
+      },
+      getCityName() {
+        var _this = this;
+        var myAmapFun = new amapFile.AMapWX({
+          key: "e545e7f79a643f23aef187add14e4548"
         });
-      } else {
+        myAmapFun.getRegeo({
+          success: function (data) {
+            //成功回调
+            console.log(data);
+            // data[0].regeocodeData.formatted_address
+            _this.cityName = data[0].regeocodeData.formatted_address;
+          },
+          fail: function (info) {
+            //失败回调
+            console.log(info);
+            //如果用户拒绝授权
+            // 默认为北京
+            _this.cityName = "北京市";
+          }
+        });
+      },
+      toSearch() {
         wx.navigateTo({
-          url: "/pages/newgoods/main?isNew=" + 1
+          url: "/pages/search/main"
+        });
+      },
+      async getData() {
+        const data = await get("/index/index");
+        this.banner = data.banner;
+        this.channel = data.channel;
+        this.brandList = data.brandList;
+        this.newGoods = data.newGoods;
+        this.hotGoods = data.hotGoods;
+        this.topicList = data.topicList;
+        this.newCategoryList = data.newCategoryList;
+      },
+      goodsDetail(id) {
+        wx.navigateTo({
+          url: "/pages/goods/main?id=" + id
+        });
+      },
+      categoryList(id) {
+        wx.navigateTo({
+          url: "/pages/categorylist/main?id=" + id
+        });
+      },
+      goodsList(info) {
+        if (info == "hot") {
+          wx.navigateTo({
+            url: "/pages/newgoods/main?isHot=" + 1
+          });
+        } else {
+          wx.navigateTo({
+            url: "/pages/newgoods/main?isNew=" + 1
+          });
+        }
+      },
+      topicdetail(id) {
+        wx.navigateTo({
+          url: "/pages/topicdetail/main?id=" + id
+        });
+      },
+      totopic() {
+        wx.navigateTo({
+          url: "/pages/topic/main"
+        });
+      },
+      tobrandList() {
+        wx.navigateTo({
+          url: "/pages/brandlist/main"
+        });
+      },
+      branddetail(id) {
+        wx.navigateTo({
+          url: "/pages/branddetail/main?id=" + id
         });
       }
     },
-    topicdetail(id) {
-      wx.navigateTo({ url: "/pages/topicdetail/main?id=" + id });
-    },
-    totopic() {
-      wx.navigateTo({ url: "/pages/topic/main" });
-    },
-    tobrandList() {
-      wx.navigateTo({ url: "/pages/brandlist/main" });
-    },
-    branddetail(id) {
-      wx.navigateTo({ url: "/pages/branddetail/main?id=" + id });
-    }
-  },
-  created() {}
-};
+    created() {}
+  };
+
 </script>
 
 <style lang='scss' scoped>
-@import "./style.scss";
+  @import "./style.scss";
+
 </style>
--- src/pages/mappage/index.vue
+++ src/pages/mappage/index.vue
@@ -3,122 +3,129 @@
     <div class="section">
       <input @input="bindInput" placeholder="搜索" v-model="keywords" focus="true" />
     </div>
-    <scroll>
+    <scroll-view :scroll-y="true" class="addcont" style="height: 500rpx;">
       <div class="result" @touchstart="bindSearch(item.name)" v-for="(item, index) in tips" :key="index">
         {{item.name}}
       </div>
-    </scroll>
+    </scroll-view>
 
-    <view class="map_container">
+    <div class="map_container">
+      <div class="title">
+        显示当前位置:
+      </div>
       <map class="map" id="map" :longitude="longitude" :latitude="latitude" scale="16" :markers="markers"></map>
-    </view>
+    </div>
   </div>
 </template>
 
 <script>
-import amapFile from "../../utils/amap-wx";
-export default {
-  onLoad() {
-    this.initData();
-    this.getMapaddress();
-  },
-  created() {},
-  mounted() {},
-  data() {
-    return {
-      tips: [],
-      keywords: "",
-      longitude: "",
-      latitude: "",
-      markers: []
-    };
-  },
-  components: {},
-  methods: {
-    bindInput: function(e) {
-      var _this = this;
-      var keywords = this.keywords;
-      var myAmapFun = new amapFile.AMapWX({ key: "e545e7f79a643f23aef187add14e4548" });
-      myAmapFun.getInputtips({
-        keywords: keywords,
-        location: "",
-        success: function(data) {
-          console.log(data);
+  import amapFile from "../../utils/amap-wx";
+  export default {
+    onLoad() {
+      this.initData();
+      this.getMapaddress();
+    },
+    created() {},
+    mounted() {},
+    data() {
+      return {
+        tips: [],
+        keywords: "",
+        longitude: "",
+        latitude: "",
+        markers: []
+      };
+    },
+    components: {},
+    methods: {
+      bindInput: function (e) {
+        var _this = this;
+        var keywords = this.keywords;
+        var myAmapFun = new amapFile.AMapWX({
+          key: "e545e7f79a643f23aef187add14e4548"
+        });
+        myAmapFun.getInputtips({
+          keywords: keywords,
+          location: "",
+          success: function (data) {
+            console.log(data);
 
-          if (data && data.tips) {
-            _this.tips = data.tips;
+            if (data && data.tips) {
+              _this.tips = data.tips;
+            }
           }
-        }
-      });
-    },
-    bindSearch: function(e) {
-      console.log(e);
-      wx.setStorageSync("cityName", e);
-      wx.navigateBack({
-        delta: 1 //返回的页面数，如果 delta 大于现有页面数，则返回到首页,
-      });
-    },
-    getMapaddress() {
-      var _this = this;
-      var myAmapFun = new amapFile.AMapWX({ key: "e545e7f79a643f23aef187add14e4548" });
-      myAmapFun.getRegeo({
-        iconPath: "/static/images/marker.png",
-        iconWidth: 22,
-        iconHeight: 32,
-        success: function(data) {
-          console.log(data);
-          var marker = [
-            {
+        });
+      },
+      bindSearch: function (e) {
+        console.log(e);
+        wx.setStorageSync("cityName", e);
+        wx.navigateBack({
+          delta: 1 //返回的页面数，如果 delta 大于现有页面数，则返回到首页,
+        });
+      },
+      getMapaddress() {
+        var _this = this;
+        var myAmapFun = new amapFile.AMapWX({
+          key: "e545e7f79a643f23aef187add14e4548"
+        });
+        myAmapFun.getRegeo({
+          iconPath: "/static/images/marker.png",
+          iconWidth: 22,
+          iconHeight: 32,
+          success: function (data) {
+            console.log(data);
+            var marker = [{
               id: data[0].id,
               latitude: data[0].latitude,
               longitude: data[0].longitude,
               iconPath: data[0].iconPath,
               width: data[0].width,
               height: data[0].height
-            }
-          ];
-          _this.markers = marker;
-          _this.latitude = data[0].latitude;
-          _this.longitude = data[0].longitude;
-          // var marker = [
-          //   {
-          //     id: data[0].id,
-          //     latitude: data[0].latitude,
-          //     longitude: data[0].longitude,
-          //     iconPath: data[0].iconPath,
-          //     width: data[0].width,
-          //     height: data[0].height
-          //   }
-          // ];
-          // that.setData({
-          //   markers: marker
-          // });
-          // that.setData({
-          //   latitude: data[0].latitude
-          // });
-          // that.setData({
-          //   longitude: data[0].longitude
-          // });
-          // that.setData({
-          //   textData: {
-          //     name: data[0].name,
-          //     desc: data[0].desc
-          //   }
-          // });
-        },
-        fail: function(info) {
-          // wx.showModal({title:info.errMsg})
-        }
-      });
+            }];
+            _this.markers = marker;
+            _this.latitude = data[0].latitude;
+            _this.longitude = data[0].longitude;
+            // var marker = [
+            //   {
+            //     id: data[0].id,
+            //     latitude: data[0].latitude,
+            //     longitude: data[0].longitude,
+            //     iconPath: data[0].iconPath,
+            //     width: data[0].width,
+            //     height: data[0].height
+            //   }
+            // ];
+            // that.setData({
+            //   markers: marker
+            // });
+            // that.setData({
+            //   latitude: data[0].latitude
+            // });
+            // that.setData({
+            //   longitude: data[0].longitude
+            // });
+            // that.setData({
+            //   textData: {
+            //     name: data[0].name,
+            //     desc: data[0].desc
+            //   }
+            // });
+          },
+          fail: function (info) {
+            // wx.showModal({title:info.errMsg})
+          }
+        });
+      },
+      initData() {
+        this.tips = [];
+        this.keywords = "";
+      }
     },
-    initData() {
-      this.tips = [];
-      this.keywords = "";
-    }
-  },
-  computed: {}
-};
+    computed: {}
+  };
+
 </script>
 <style lang='scss' scoped>
-@import "./style";
+  @import "./style";
+
 </style>
