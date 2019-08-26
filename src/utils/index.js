239a374fe7540c463292470601c9895a12b4a606
heyushuo <1007561607@qq.com>
2019-08-26 11:01:11 +0800

    域名到期了，现在请求接口换成ip了

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,72 +1,67 @@
 function formatNumber(n) {
-  const str = n.toString()
-  return str[1] ? str : `0${str}`
+  const str = n.toString();
+  return str[1] ? str : `0${str}`;
 }
 
 export function formatTime(date) {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
 
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
-  const t1 = [year, month, day].map(formatNumber).join('/')
-  const t2 = [hour, minute, second].map(formatNumber).join(':')
+  const t1 = [year, month, day].map(formatNumber).join("/");
+  const t2 = [hour, minute, second].map(formatNumber).join(":");
 
-  return `${t1} ${t2}`
+  return `${t1} ${t2}`;
 }
-
 
 //-------------------------------------------------------------------------请求的封装
 
-const host = 'https://www.heyuhsuo.xyz/heyushuo';
-export {
-  host
-}
+const host = "http://118.25.222.68:5757/heyushuo";
+export { host };
 //请求封装
 function request(url, method, data, header = {}) {
   wx.showLoading({
-    title: '加载中' //数据请求前loading
-  })
+    title: "加载中" //数据请求前loading
+  });
   return new Promise((resolve, reject) => {
     wx.request({
       url: host + url, //仅为示例，并非真实的接口地址
       method: method,
       data: data,
       header: {
-        'content-type': 'application/json' // 默认值
+        "content-type": "application/json" // 默认值
       },
-      success: function (res) {
+      success: function(res) {
         wx.hideLoading();
-        resolve(res.data)
+        resolve(res.data);
       },
-      fail: function (error) {
+      fail: function(error) {
         wx.hideLoading();
-        reject(false)
+        reject(false);
       },
-      complete: function () {
+      complete: function() {
         wx.hideLoading();
       }
-    })
-  })
+    });
+  });
 }
 export function get(url, data) {
-  return request(url, 'GET', data)
+  return request(url, "GET", data);
 }
 export function post(url, data) {
-  return request(url, 'POST', data)
+  return request(url, "POST", data);
 }
 
 //-------------------------------------------------------------------------请求的封装
 
-
 //----------------------------------------------用户是否登录 未登录跳转到登录页面 -------------------------
 
-
 export function toLogin() {
-  const userInfo = wx.getStorageSync('userInfo');
+  const userInfo = wx.getStorageSync("userInfo");
   if (!userInfo) {
     wx.navigateTo({
       url: "/pages/login/main"
@@ -77,7 +72,7 @@ export function toLogin() {
 }
 
 export function login() {
-  const userInfo = wx.getStorageSync('userInfo');
+  const userInfo = wx.getStorageSync("userInfo");
   if (userInfo) {
     return userInfo;
   }
@@ -85,18 +80,14 @@ export function login() {
 
 //----------------------------------------------用户是否登录 未登录跳转到登录页面 -------------------------
 
-
 export function getStorageOpenid() {
   const openId = wx.getStorageSync("openId");
   if (openId) {
     return openId;
   } else {
-    return ''
+    return "";
   }
 }
-
-
-
 
 export function getOpenid() {
   // wx.login({
@@ -119,7 +110,6 @@ export function getOpenid() {
   //     } else {
   //       console.log('登录失败！' + res.errMsg)
   //     }
-
   //   },
   //   fail: () => {},
   //   complete: () => {}
